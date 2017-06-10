6578bf05c24d30615c48aebec59e6d899d8f22e0
dart-wakar <wakar@whitelogic.in>
2017-06-11 05:06:26 +0530

    Basic FB login for the app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FacebookService,LoginResponse,LoginOptions } from 'ngx-facebook';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,55 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+
+  fbId:any;
+  accessToken:any;
+  user:any;
+
+  constructor(private fb: FacebookService) {
+    console.log("Initializing Facebook");
+
+    fb.init({
+      appId: '300039560455517',
+      xfbml: true,
+      version: 'v2.9'
+    });
+  }
+
+  login() {
+    this.fb.login().then((res:LoginResponse) => {
+      console.log('Logged in ',res);
+      this.fbId = res.authResponse.userID;
+      this.accessToken = res.authResponse.accessToken;
+      console.log('User id',this.fbId);
+      this.getProfile()
+    })
+    .catch(this.handleError);
+  }
+
+  private handleError(error) {
+    console.error('Error processing action',error);
+  }
+
+  getProfile() {
+    this.fb.api('/me?fields=id,name,first_name,last_name,gender,link,birthday,email')
+      .then((res:any) => {
+        console.log('Got the users profile',res);
+        this.user = res;
+        console.log(this.user.gender);
+        localStorage.clear()
+        localStorage.setItem("fbId",this.user.id)
+        localStorage.setItem("name",this.user.name)
+        localStorage.setItem("gender",this.user.gender)
+      })
+      .catch(this.handleError);
+  }
+
+  logout() {
+    this.fb.logout().then((res:any) => {
+      console.log(res);
+    })
+    .catch(this.handleError);
+  }
+
 }
