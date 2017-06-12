c3025fb004e7baca894433df1c4d098a499e3dcc
dart-wakar <wakar@whitelogic.in>
2017-06-12 08:40:45 +0530

    Buy product workflow

--- src/app/components/product-details/product-details.component.ts
+++ src/app/components/product-details/product-details.component.ts
@@ -3,6 +3,7 @@ import {ActivatedRoute,Params,Router} from '@angular/router';
 import {Location} from '@angular/common';
 import {ProductService} from '../../services/product.service';
 import 'rxjs/add/operator/switchMap';
+import * as _ from 'lodash';
 
 @Component({
     selector: 'product-details',
@@ -12,7 +13,9 @@ import 'rxjs/add/operator/switchMap';
 export class ProductDetailsComponent implements OnInit,OnChanges {
 
     ifCreatedBy: boolean;
+    ifAlreadyBought: boolean;
     ifCreatedByResolved: boolean;
+    ifAlreadyBoughtResolved: boolean;
     product:any;
     viewMode: boolean;
 
@@ -39,6 +42,7 @@ export class ProductDetailsComponent implements OnInit,OnChanges {
                 console.log(product);
                 this.product = product;
                 this.resolveIfSeller(product);
+                this.resolveIfAlreadyBought(product);
             },err => console.log(err));
     }
 
@@ -53,6 +57,19 @@ export class ProductDetailsComponent implements OnInit,OnChanges {
         console.log(this.ifCreatedBy);
     }
 
+    resolveIfAlreadyBought(product:any) {
+        let current_user_id = localStorage.getItem('current_user_id');
+        var x = this;
+        this.ifAlreadyBought = false;
+        _.forEach(product.buyers,function(buyer) {
+            console.log(buyer);
+            if(current_user_id == buyer) {
+                x.ifAlreadyBought = true;
+            }
+        });
+        this.ifAlreadyBoughtResolved = true;
+    }
+
     goBack() {
         this.location.back();
     }
@@ -64,5 +81,13 @@ export class ProductDetailsComponent implements OnInit,OnChanges {
     onEditToView(flag:boolean) {
         this.viewMode = flag;
         this.getProductDetailsFromRoute();
+    }
+
+    buyProduct() {
+        this.productService.buyProduct(localStorage.getItem('fb_id'),this.product._id)
+            .subscribe(product => {
+                console.log(product);
+                this.getProductDetailsFromRoute();
+            },err => console.log(err));
     }
 }
