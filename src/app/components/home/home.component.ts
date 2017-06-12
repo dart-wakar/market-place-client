a4e49dceab35264c56bedef6397356911a23b622
dart-wakar <wakar@whitelogic.in>
2017-06-12 09:19:03 +0530

    Facebook logout implemented

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,5 +1,6 @@
 import {Component,OnInit} from '@angular/core';
 import {Router} from '@angular/router';
+import {FacebookService,LoginResponse,LoginOptions} from 'ngx-facebook';
 
 @Component({
     selector: 'home',
@@ -9,9 +10,30 @@ import {Router} from '@angular/router';
 export class HomeComponent {
     componentName = 'Home'
 
-    constructor(private router: Router) {}
+    constructor(private router: Router,private fb: FacebookService) {
+        console.log('Initializing Facebook');
+
+        fb.init({
+            appId: '300039560455517',
+            xfbml: true,
+            version: 'v2.9'
+        });
+    }
 
     addNewProduct(){
         this.router.navigate(['/products/add']);
+    }
+
+    logout() {
+        this.fb.logout().then((res:any) => {
+            console.log(res);
+            localStorage.clear();
+            this.router.navigate(['/test']);
+        })
+        .catch(this.handleError);
+    }
+
+    private handleError(error) {
+        console.error('Error processing action',error);
     }
 }
